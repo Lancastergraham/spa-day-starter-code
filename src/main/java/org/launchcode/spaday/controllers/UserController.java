81be8dafc118f298829501aaffbfb693cf56f700
Lancaster Graham <Lancastergraham87@gmail.com>
2021-06-03 21:00:45 -0500

    Finished studio

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,33 +4,35 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping
-    public String userHello(Model model) {
-        model.addAttribute("title", "Welcome to User Index");
-        return "user/index";
-    }
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
 
-    //Create Handler method to render the form
-    @GetMapping("add")
-    public String displayAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("title", "Add a user");
-        model.addAttribute(new User());
+        model.addAttribute("title", "Add User");
 
         return "user/add";
     }
 
-    @PostMapping("add")
-    public String processAddUserForm() {
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user,
+                                     String verify) {
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
 
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            model.addAttribute("error", "Passwords do not match");
+            return "user/add";
+        }
     }
+
 
 }
